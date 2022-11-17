4962ec48370b61e14fc67a7d92e9dea0f98ebbba
djumelle <82345394+djumelle@users.noreply.github.com>
2022-11-17 01:05:40 -0300

    Add files via upload

---
+++ calculadora-lpoo/Form1.Designer.vb
@@ -0,0 +1,311 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BC = New System.Windows.Forms.Button()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.B4 = New System.Windows.Forms.Button()
+        Me.B5 = New System.Windows.Forms.Button()
+        Me.B6 = New System.Windows.Forms.Button()
+        Me.B9 = New System.Windows.Forms.Button()
+        Me.B7 = New System.Windows.Forms.Button()
+        Me.B8 = New System.Windows.Forms.Button()
+        Me.B0 = New System.Windows.Forms.Button()
+        Me.BDel = New System.Windows.Forms.Button()
+        Me.BSub = New System.Windows.Forms.Button()
+        Me.BEq = New System.Windows.Forms.Button()
+        Me.BSum = New System.Windows.Forms.Button()
+        Me.Display = New System.Windows.Forms.TextBox()
+        Me.Display2 = New System.Windows.Forms.TextBox()
+        Me.BMul = New System.Windows.Forms.Button()
+        Me.BDiv = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BC
+        '
+        Me.BC.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.BC.Location = New System.Drawing.Point(10, 232)
+        Me.BC.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BC.Name = "BC"
+        Me.BC.Size = New System.Drawing.Size(64, 41)
+        Me.BC.TabIndex = 0
+        Me.BC.Text = "C"
+        Me.BC.UseVisualStyleBackColor = True
+        '
+        'B1
+        '
+        Me.B1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.B1.Location = New System.Drawing.Point(10, 187)
+        Me.B1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(64, 41)
+        Me.B1.TabIndex = 1
+        Me.B1.Text = "1"
+        Me.B1.UseVisualStyleBackColor = True
+        '
+        'B2
+        '
+        Me.B2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.B2.Location = New System.Drawing.Point(78, 187)
+        Me.B2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.B2.Name = "B2"
+        Me.B2.Size = New System.Drawing.Size(64, 41)
+        Me.B2.TabIndex = 2
+        Me.B2.Text = "2"
+        Me.B2.UseVisualStyleBackColor = True
+        '
+        'B3
+        '
+        Me.B3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.B3.Location = New System.Drawing.Point(146, 187)
+        Me.B3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.B3.Name = "B3"
+        Me.B3.Size = New System.Drawing.Size(64, 41)
+        Me.B3.TabIndex = 3
+        Me.B3.Text = "3"
+        Me.B3.UseVisualStyleBackColor = True
+        '
+        'B4
+        '
+        Me.B4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.B4.Location = New System.Drawing.Point(10, 142)
+        Me.B4.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.B4.Name = "B4"
+        Me.B4.Size = New System.Drawing.Size(64, 41)
+        Me.B4.TabIndex = 4
+        Me.B4.Text = "4"
+        Me.B4.UseVisualStyleBackColor = True
+        '
+        'B5
+        '
+        Me.B5.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.B5.Location = New System.Drawing.Point(78, 142)
+        Me.B5.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.B5.Name = "B5"
+        Me.B5.Size = New System.Drawing.Size(64, 41)
+        Me.B5.TabIndex = 5
+        Me.B5.Text = "5"
+        Me.B5.UseVisualStyleBackColor = True
+        '
+        'B6
+        '
+        Me.B6.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.B6.Location = New System.Drawing.Point(146, 142)
+        Me.B6.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.B6.Name = "B6"
+        Me.B6.Size = New System.Drawing.Size(64, 41)
+        Me.B6.TabIndex = 6
+        Me.B6.Text = "6"
+        Me.B6.UseVisualStyleBackColor = True
+        '
+        'B9
+        '
+        Me.B9.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.B9.Location = New System.Drawing.Point(146, 96)
+        Me.B9.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.B9.Name = "B9"
+        Me.B9.Size = New System.Drawing.Size(64, 41)
+        Me.B9.TabIndex = 7
+        Me.B9.Text = "9"
+        Me.B9.UseVisualStyleBackColor = True
+        '
+        'B7
+        '
+        Me.B7.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.B7.Location = New System.Drawing.Point(10, 96)
+        Me.B7.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.B7.Name = "B7"
+        Me.B7.Size = New System.Drawing.Size(64, 41)
+        Me.B7.TabIndex = 7
+        Me.B7.Text = "7"
+        Me.B7.UseVisualStyleBackColor = True
+        '
+        'B8
+        '
+        Me.B8.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.B8.Location = New System.Drawing.Point(78, 96)
+        Me.B8.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.B8.Name = "B8"
+        Me.B8.Size = New System.Drawing.Size(64, 41)
+        Me.B8.TabIndex = 8
+        Me.B8.Text = "8"
+        Me.B8.UseVisualStyleBackColor = True
+        '
+        'B0
+        '
+        Me.B0.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.B0.Location = New System.Drawing.Point(78, 232)
+        Me.B0.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.B0.Name = "B0"
+        Me.B0.Size = New System.Drawing.Size(64, 41)
+        Me.B0.TabIndex = 9
+        Me.B0.Text = "0"
+        Me.B0.UseVisualStyleBackColor = True
+        '
+        'BDel
+        '
+        Me.BDel.Location = New System.Drawing.Point(214, 96)
+        Me.BDel.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BDel.Name = "BDel"
+        Me.BDel.Size = New System.Drawing.Size(64, 41)
+        Me.BDel.TabIndex = 10
+        Me.BDel.Text = "⌫"
+        Me.BDel.UseVisualStyleBackColor = True
+        '
+        'BSub
+        '
+        Me.BSub.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.BSub.Location = New System.Drawing.Point(146, 232)
+        Me.BSub.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BSub.Name = "BSub"
+        Me.BSub.Size = New System.Drawing.Size(64, 41)
+        Me.BSub.TabIndex = 11
+        Me.BSub.Text = "-"
+        Me.BSub.UseVisualStyleBackColor = True
+        '
+        'BEq
+        '
+        Me.BEq.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.BEq.Location = New System.Drawing.Point(214, 277)
+        Me.BEq.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BEq.Name = "BEq"
+        Me.BEq.Size = New System.Drawing.Size(64, 41)
+        Me.BEq.TabIndex = 12
+        Me.BEq.Text = "="
+        Me.BEq.UseVisualStyleBackColor = True
+        '
+        'BSum
+        '
+        Me.BSum.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.BSum.Location = New System.Drawing.Point(214, 232)
+        Me.BSum.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BSum.Name = "BSum"
+        Me.BSum.Size = New System.Drawing.Size(64, 41)
+        Me.BSum.TabIndex = 13
+        Me.BSum.Text = "+"
+        Me.BSum.UseVisualStyleBackColor = True
+        '
+        'Display
+        '
+        Me.Display.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.Display.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.Display.Font = New System.Drawing.Font("Consolas", 18.0!)
+        Me.Display.Location = New System.Drawing.Point(30, 42)
+        Me.Display.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Display.Name = "Display"
+        Me.Display.Size = New System.Drawing.Size(242, 29)
+        Me.Display.TabIndex = 14
+        Me.Display.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Display2
+        '
+        Me.Display2.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.Display2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.Display2.Enabled = False
+        Me.Display2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Display2.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Display2.Location = New System.Drawing.Point(22, 22)
+        Me.Display2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Display2.Name = "Display2"
+        Me.Display2.Size = New System.Drawing.Size(256, 16)
+        Me.Display2.TabIndex = 15
+        Me.Display2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'BMul
+        '
+        Me.BMul.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.BMul.Location = New System.Drawing.Point(214, 187)
+        Me.BMul.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BMul.Name = "BMul"
+        Me.BMul.Size = New System.Drawing.Size(64, 41)
+        Me.BMul.TabIndex = 16
+        Me.BMul.Text = "x"
+        Me.BMul.UseVisualStyleBackColor = True
+        '
+        'BDiv
+        '
+        Me.BDiv.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.BDiv.Location = New System.Drawing.Point(214, 142)
+        Me.BDiv.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BDiv.Name = "BDiv"
+        Me.BDiv.Size = New System.Drawing.Size(64, 41)
+        Me.BDiv.TabIndex = 17
+        Me.BDiv.Text = "÷"
+        Me.BDiv.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.ClientSize = New System.Drawing.Size(288, 354)
+        Me.Controls.Add(Me.BDiv)
+        Me.Controls.Add(Me.BMul)
+        Me.Controls.Add(Me.Display2)
+        Me.Controls.Add(Me.Display)
+        Me.Controls.Add(Me.BSum)
+        Me.Controls.Add(Me.BEq)
+        Me.Controls.Add(Me.BSub)
+        Me.Controls.Add(Me.BDel)
+        Me.Controls.Add(Me.B0)
+        Me.Controls.Add(Me.B8)
+        Me.Controls.Add(Me.B7)
+        Me.Controls.Add(Me.B9)
+        Me.Controls.Add(Me.B6)
+        Me.Controls.Add(Me.B5)
+        Me.Controls.Add(Me.B4)
+        Me.Controls.Add(Me.B3)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.BC)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BC As Button
+    Friend WithEvents B1 As Button
+    Friend WithEvents B2 As Button
+    Friend WithEvents B3 As Button
+    Friend WithEvents B4 As Button
+    Friend WithEvents B5 As Button
+    Friend WithEvents B6 As Button
+    Friend WithEvents B9 As Button
+    Friend WithEvents B7 As Button
+    Friend WithEvents B8 As Button
+    Friend WithEvents B0 As Button
+    Friend WithEvents BDel As Button
+    Friend WithEvents BSub As Button
+    Friend WithEvents BEq As Button
+    Friend WithEvents BSum As Button
+    Friend WithEvents Display As TextBox
+    Friend WithEvents Display2 As TextBox
+    Friend WithEvents BMul As Button
+    Friend WithEvents BDiv As Button
+End Class
